08d043f227f34abd12950c06850392e7cfb3cfaa
Asad939asad <asadirfan939@gmail.com>
2026-06-03 16:40:52 +0500

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/Portfolio---asadirfan7533/',
   plugins: [
     react(),
     tailwindcss(),
